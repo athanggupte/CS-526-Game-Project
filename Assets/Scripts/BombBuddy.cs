4461a801ce27393aa00d89741c7827ead449ce6f
AsianIntel <gautam.abhyankar@gmail.com>
2023-10-09 17:31:03 -0700

    Add Static Enemy Bomb

---
+++ Assets/Scripts/BombBuddy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombBuddy : MonoBehaviour
+{
+    public Sprite BombSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = BombSprite;
+            gameObject.GetComponent<ColorBombEffector>().Deploy();
+        }        
+    }
+}
