73538359c2fb7e4f53b4090de8469dfdcace95ae
goharzeb <manki8466@gmail.com>
2023-10-31 11:24:58 -0700

    integrated hud overlay scene with levels

---
+++ Assets/Scripts/HudOverlay.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HudOverlay : MonoBehaviour
+{
+    [SerializeField] private int hudSceneIndex = 1; // Assuming HUD scene is at index 1 in Build Settings
+
+    void Start()
+    {
+        // Check if the HUD scene is already loaded (optional but can be efficient)
+        if (!SceneManager.GetSceneByBuildIndex(hudSceneIndex).isLoaded)
+        {
+            // Load the HUD scene additively using its index
+            SceneManager.LoadScene(hudSceneIndex, LoadSceneMode.Additive);
+        }
+    }
+}
