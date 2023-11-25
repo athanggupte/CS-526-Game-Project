40fe15f6f7194d998d215708e33bc0ee298e4d24
Athang Gupte <xdevapps0@gmail.com>
2023-11-25 04:04:18 -0800

    Show trajectory only when ammo is available

--- Assets/Scripts/GunWeaponHandler.cs
+++ Assets/Scripts/GunWeaponHandler.cs
@@ -19,7 +19,15 @@ public class GunWeaponHandler : MonoBehaviour
 
     void Update()
     {
-        DrawTrajectory();
+        if (ammoCount > 0)
+        {
+            m_lineRenderer.enabled = true;
+            DrawTrajectory();
+        }
+        else
+        {
+            m_lineRenderer.enabled = false;
+        }
     }
 
     public void SetAmmo(int count)
